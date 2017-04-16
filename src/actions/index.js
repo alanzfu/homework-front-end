586f1c09f0f0312df47b47050c335cd1fee897e6
Alan Fu <alanzfu@gmail.com>
2017-04-16 10:09:47 -0700

    adds sort actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,38 @@
 'use strict';
-const actions = require('../constants').action;
+const actions = require('../constants').actions;
+const request = require('request');
+
+
+/*
+  Ajax request to get REPOs from ghAPI
+*/
+
+const getReposByHandle = payload => {
+  return function(dispatch){
+    const url = ` https://api.github.com/users/${payload.githubHandle}/repos`;
+    request(url, (err, resp, body) => {
+      if (err) {
+        return dispatch({
+          type: actions.GET_REPO_ERROR
+        });
+      }
+
+      dispatch({
+        type: actions.GET_REPO_SUCCESS,
+        payload: JSON.parse(body);
+      });
+    });
+};
+
+const sortRepos = payload => {
+  return {
+    type: actions.SORT,
+    payload: payload
+  };
+}
+
+
+module.exports = {
+  getReposByHandle: getReposByHandle,
+  sortRepos: sortRepos
+}
